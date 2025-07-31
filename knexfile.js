d025e6f5d682bc16b4a40f2fca757bc609f792c3
Gabriel N. V. Resende <62761033+gnvr29@users.noreply.github.com>
2025-07-31 13:00:28 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+      host: 'postgres',
       port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
